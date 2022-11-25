a68f32129962162cf84fbc30a9b600aef320eaac
cameo choquer <cjchoquer@gmail.com>
2022-11-25 20:53:27 +0000

    THE LOOPING WORKS!

--- proj_2.js
+++ proj_2.js
@@ -12,8 +12,21 @@
    
     var btn = document.querySelectorAll('button');
     var buttons = [...btn]
-    console.log(photos)
+    //console.log(photos)
 
+
+    for (var i = 0; i < images.length; i++) {
+      (function(index) {
+           buttons[index].addEventListener("click", function() {
+            let x = images[index];
+            if (x.style.display === 'none') {
+                 x.style.display = 'flex';
+              } else {
+              x.style.display = 'none';
+              }
+            })
+      })(i);
+   }
   //   for(var photo = 0; photo < photos.length; photo++) {
     
   //     btn[0].addEventListener("click", function(e){ 
