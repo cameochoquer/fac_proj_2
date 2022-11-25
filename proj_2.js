bff43d489957feb86b3bb2b70a52ea99d580101b
cameo choquer <cjchoquer@gmail.com>
2022-11-25 20:45:24 +0000

    looping through buttons and images

--- proj_2.js
+++ proj_2.js
@@ -1,8 +1,30 @@
-function toggle1() {
-    var x = document.getElementById('proj1');
-    if (x.style.display === 'none') {
-      x.style.display = 'flex';
-    } else {
-      x.style.display = 'none';
-    }
-  }
+	// function toggle1() {
+	//     var x = document.getElementById('proj1');
+	//     if (x.style.display === 'none') {
+	//       x.style.display = 'flex';
+	//     } else {
+	//       x.style.display = 'none';
+	//     }
+	//   };
+
+    var photos = document.querySelectorAll('img');
+    var images =[...photos]
+   
+    var btn = document.querySelectorAll('button');
+    var buttons = [...btn]
+    console.log(photos)
+
+  //   for(var photo = 0; photo < photos.length; photo++) {
+    
+  //     btn[0].addEventListener("click", function(e){ 
+  //     for(var i=0;i<btn.length;i++{
+  //       btn[i.addEventListener('click',)]
+  //     });
+  //     var x = document.getElementsByClassName('projImg');
+  //     if (x.style.display === 'none') {
+  //     x.style.display = 'flex';
+  //   } else {
+  //     x.style.display = 'none';
+  //   };
+  // });
+  //   };
