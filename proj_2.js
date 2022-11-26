0db93693da8bb36a8a8a2205cde93c0e7d6a9ba6
cameo choquer <cjchoquer@gmail.com>
2022-11-26 22:51:24 +0000

    removed commented Lines

--- proj_2.js
+++ proj_2.js
@@ -1,18 +1,11 @@
-	// function toggle1() {
-	//     var x = document.getElementById('proj1');
-	//     if (x.style.display === 'none') {
-	//       x.style.display = 'flex';
-	//     } else {
-	//       x.style.display = 'none';
-	//     }
-	//   };
+
 
     var photos = document.querySelectorAll('img');
     var images =[...photos]
    
     var btn = document.querySelectorAll('button');
     var buttons = [...btn]
-    //console.log(photos)
+
 
 
     for (var i = 0; i < images.length; i++) {
@@ -27,17 +20,3 @@
             })
       })(i);
    }
-  //   for(var photo = 0; photo < photos.length; photo++) {
-    
-  //     btn[0].addEventListener("click", function(e){ 
-  //     for(var i=0;i<btn.length;i++{
-  //       btn[i.addEventListener('click',)]
-  //     });
-  //     var x = document.getElementsByClassName('projImg');
-  //     if (x.style.display === 'none') {
-  //     x.style.display = 'flex';
-  //   } else {
-  //     x.style.display = 'none';
-  //   };
-  // });
-  //   };
